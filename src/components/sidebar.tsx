a6a3c178b1b18d7aea4fd163b70d9ac753f20f79
matsubrasil <matsuboy@outlook.com>
2024-03-02 11:16:53 -0300

    feat: add  items menu

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,12 +1,95 @@
 'use client'
 
-import UserItem from "./userItem"
+import {
+  Command,
+  CommandList,
+  CommandGroup,
+  CommandItem,
+} from '@/components/ui/command'
+import UserItem from './userItem'
+import {
+  Bell,
+  Cookie,
+  CreditCard,
+  Inbox,
+  Menu,
+  MessageSquare,
+  Settings,
+  User,
+} from 'lucide-react'
 
 export default function Sidebar() {
+  const menuList = [
+    {
+      group: 'General',
+      items: [
+        {
+          link: '/',
+          text: 'Profile',
+          icon: <User />,
+        },
+        {
+          link: '/',
+          text: 'Inbox',
+          icon: <Inbox />,
+        },
+        {
+          link: '/',
+          text: 'Billing',
+          icon: <CreditCard />,
+        },
+        {
+          link: '/',
+          text: 'Notifications',
+          icon: <Bell />,
+        },
+      ],
+    },
+    {
+      group: 'Settings',
+      items: [
+        {
+          link: '/',
+          text: 'General Settings',
+          icon: <Settings />,
+        },
+        {
+          link: '/',
+          text: 'Privacy',
+          icon: <Cookie />,
+        },
+        {
+          link: '/',
+          text: 'Logs',
+          icon: <MessageSquare />,
+        },
+      ],
+    },
+  ]
   return (
     <div className="flex min-h-screen w-[300px] min-w-[300px] flex-col gap-2 border-r p-4">
-      <div><UserItem /></div>
-      <div className="grow">Menu</div>
+      <div>
+        <UserItem />
+      </div>
+      <div className="grow">
+        <Command style={{ overflow: 'visible' }}>
+          <CommandList style={{ overflow: 'visible' }}>
+            {menuList.map((menu) => (
+              <CommandGroup key={menu.group} heading={menu.group}>
+                {menu.items.map((option) => (
+                  <CommandItem
+                    key={option.text}
+                    className="flex items-center justify-start gap-2 cursor-pointer"
+                  >
+                    {option.icon}
+                    {option.text}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            ))}
+          </CommandList>
+        </Command>
+      </div>
       <div>Settings / Notifications</div>
     </div>
   )
